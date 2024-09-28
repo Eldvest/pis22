f76f1ca37b6274d2a00d1d2ce610454b852cac46
Eldvest <anastasuasuhanova05@gmail.com>
2024-09-28 15:28:26 +0700

    класс Program

--- основы проектирования/Program.cs
+++ основы проектирования/Program.cs
@@ -2,21 +2,17 @@
 using System.Text.RegularExpressions;
 using основы_проектирования;
 
-    string str ="Файлы: \'ghbdtn vbh\' ; 2024.02.02; 23;"; 
-    Console.Write(str);
-    //str = str.Replace(" ", "");
-    str = str.Replace(":", ";");
-    string[] words = str.Split(';', StringSplitOptions.RemoveEmptyEntries);
+string fileString = "VideoFile; name1строка;20.02.02; 20; 02:30;\n" +
+                   "TextFile; name2строка;20.04.02; 11; txt\n" +
+                   "TextFile; name3строка;20.04.02; 11; txt";
 
-try
-{
-    DateTime parsedDate = DateTime.Parse(words[2]);
-    if ((words.Length == 4 && words[1].Trim().StartsWith("\'") && words[1].Trim().EndsWith("\'")) && ((int.TryParse(words[3], out int value))))
-    {
-        Objectt obj = new Objectt(words[0], words[1], parsedDate, Convert.ToInt32(words[3]));
-        Console.WriteLine(obj.PrintInf());
-    }
-    else { Console.WriteLine("Некорректные данные"); }
-}
-catch (System.FormatException) { Console.WriteLine("Неверный ввод даты"); }
-    
+List<Objectt> objects = Create.ParseString(fileString);
+foreach (Objectt obj in objects) { Console.WriteLine(obj.PrintInf()); }
+
+
+string filePath = "C:\\Users\\Настя\\source\\repos\\основы проектирования\\основы проектирования\\file.txt"; 
+List<Objectt> objectsFromFile = Create.ParseFile(filePath);
+foreach (Objectt obj in objectsFromFile) { Console.WriteLine(obj.PrintInf()); }
+
+
+
