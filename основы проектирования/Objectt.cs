d77c28bde85b81959ec4983fceabcce4e0cd4003
Eldvest <anastasuasuhanova05@gmail.com>
2024-09-28 15:16:34 +0700

    Добавьте файлы проекта.

---
+++ основы проектирования/Objectt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace основы_проектирования
+{
+    public class Objectt
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int Size { get; set; }
+
+        public Objectt(string type, string name, DateTime date, int size)
+        {
+            Type = type;
+            Name = name;
+            Date = date;
+            Size = size;
+        }
+        public string PrintInf()
+        {
+            return $"\nОбъект: {Type}\nНазвание файла - {Name}\nДата создания - {Date.ToString("yyyy.MM.dd")}\nРазмер - {Size} байт\n";
+        }
+    }
+    
+}
---
+++ основы проектирования/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Text.RegularExpressions;
+using основы_проектирования;
+
+    string str ="Файлы: \'ghbdtn vbh\' ; 2024.02.02;      23;"; 
+    Console.Write(str);
+    //str = str.Replace(" ", "");
+    str = str.Replace(":", ";");
+    string[] words = str.Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+try
+{
+    DateTime parsedDate = DateTime.Parse(words[2]);
+    if ((words.Length == 4 && words[1].Trim().StartsWith("\'") && words[1].Trim().EndsWith("\'")) && ((int.TryParse(words[3], out int value))))
+    {
+        Objectt obj = new Objectt(words[0], words[1], parsedDate, Convert.ToInt32(words[3]));
+        Console.WriteLine(obj.PrintInf());
+    }
+    else { Console.WriteLine("Некорректные данные"); }
+}
+catch (System.FormatException) { Console.WriteLine("Неверный ввод даты"); }
+    
